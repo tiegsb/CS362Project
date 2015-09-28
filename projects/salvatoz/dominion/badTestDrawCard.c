cd70ae340ca2fe15663a7d6d382c93916ca2be62
zanesalvatore <zanesalvatore@gmail.com>
2015-09-28 08:12:26 -0700

    reformat source with clang-format for readability

--- projects/salvatoz/dominion/badTestDrawCard.c
+++ projects/salvatoz/dominion/badTestDrawCard.c
@@ -10,35 +10,34 @@
 
 int checkDrawCard(int p, struct gameState *post) {
   int r;
-    
-  r = drawCard (p, post);
+
+  r = drawCard(p, post);
 }
 
-int main () {
-
+int main() {
   int i, n, r, p, deckCount, discardCount, handCount;
 
-  int k[10] = {adventurer, council_room, feast, gardens, mine,
-	       remodel, smithy, village, baron, great_hall};
+  int k[10] = {adventurer, council_room, feast,   gardens, mine,
+               remodel,    smithy,       village, baron,   great_hall};
 
   struct gameState G;
 
-  printf ("Testing drawCard.\n");
+  printf("Testing drawCard.\n");
 
-  printf ("RANDOM TESTS.\n");
+  printf("RANDOM TESTS.\n");
 
   SelectStream(2);
   PutSeed(3);
 
   for (n = 0; n < 2000; n++) {
     for (i = 0; i < sizeof(struct gameState); i++) {
-      ((char*)&G)[i] = floor(Random() * 256);
+      ((char *)&G)[i] = floor(Random() * 256);
     }
     p = floor(Random() * 1000);
     checkDrawCard(p, &G);
   }
 
-  printf ("ALL TESTS OK\n");
+  printf("ALL TESTS OK\n");
 
   exit(0);
 }
--- projects/salvatoz/dominion/testInit.c
+++ projects/salvatoz/dominion/testInit.c
@@ -3,52 +3,51 @@
 #include <stdlib.h>
 #include "rngs.h"
 
-int main (int argc, char** argv) {
-
+int main(int argc, char **argv) {
   struct gameState G;
-  
+
   int i;
 
   int start = -1;
 
-  int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, 
-	       sea_hag, tribute, smithy};
-  
+  int k[10] = {adventurer, gardens,  embargo, village, minion,
+               mine,       cutpurse, sea_hag, tribute, smithy};
+
   memset(&G, 'z', sizeof(struct gameState));
-  
+
   initializeGame(4, k, atoi(argv[1]), &G);
-  
-  printf ("Rough guide to locations in structure:\n");
-  printf ("0: numPlayers\n");
-  printf ("%ld: supplyCount[0]\n", ((long)&(G.supplyCount[0]))-((long)&G));
-  printf ("%ld: embargoTokens[0]\n", ((long)&(G.embargoTokens[0]))-((long)&G));
-  printf ("%ld: hand[0][0]\n", ((long)&(G.hand[0][0]))-(long)(&G));
-  printf ("%ld: deck[0][0]\n", ((long)&(G.deck[0][0]))-(long)(&G));
-  printf ("%ld: discard[0][0]\n", ((long)&(G.discard[0][0]))-(long)(&G));
-  printf ("%ld: playerCards[0]\n", ((long)&(G.playedCards[0]))-(long)(&G));
+
+  printf("Rough guide to locations in structure:\n");
+  printf("0: numPlayers\n");
+  printf("%ld: supplyCount[0]\n", ((long)&(G.supplyCount[0])) - ((long)&G));
+  printf("%ld: embargoTokens[0]\n", ((long)&(G.embargoTokens[0])) - ((long)&G));
+  printf("%ld: hand[0][0]\n", ((long)&(G.hand[0][0])) - (long)(&G));
+  printf("%ld: deck[0][0]\n", ((long)&(G.deck[0][0])) - (long)(&G));
+  printf("%ld: discard[0][0]\n", ((long)&(G.discard[0][0])) - (long)(&G));
+  printf("%ld: playerCards[0]\n", ((long)&(G.playedCards[0])) - (long)(&G));
 
   for (i = 0; i < sizeof(struct gameState); i++) {
-    if (((char*)&G)[i] == 'z') {
+    if (((char *)&G)[i] == 'z') {
       if (start == -1) {
-	start = i;
+        start = i;
       }
-    } else{
+    } else {
       if (start != -1) {
-	if (start == (i-1)) {
-	  printf ("Byte %d uninitialized.\n", start);
-	} else {
-	  printf ("Bytes %d-%d uninitialized.\n", start, i-1);
-	}
-	start = -1;
+        if (start == (i - 1)) {
+          printf("Byte %d uninitialized.\n", start);
+        } else {
+          printf("Bytes %d-%d uninitialized.\n", start, i - 1);
+        }
+        start = -1;
       }
     }
   }
 
   if (start != -1) {
-    if (start == (i-1)) {
-      printf ("Byte %d uninitialized.\n", start);
+    if (start == (i - 1)) {
+      printf("Byte %d uninitialized.\n", start);
     } else {
-      printf ("Bytes %d-%d uninitialized.\n", start, i-1);
+      printf("Bytes %d-%d uninitialized.\n", start, i - 1);
     }
   }
 
