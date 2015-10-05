66708b4d31ab6771c78048c5d33f49c24995c138
John R Toth <tothj@oregonstate.edu>
2015-10-05 15:51:27 -0700

    Adding refactor.c

---
+++ projects/tothj/dominion/refactor.c
@@ -0,0 +1,21 @@
+/*John R. Toth
+*CS362 Assignment 2
+*tothj@oregonstate.edu
+*refactor.c
+*/
+
+/*
+Introducing bugs into 5 cards. 
+
+Smithy
+
+Adventurer
+
+Council Room
+
+Mine
+
+Minions
+
+*/
+
