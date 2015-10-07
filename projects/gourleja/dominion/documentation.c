81e18ba04e601b3a592769606062355547c24024
Jason <jpgourley@gmail.com>
2015-10-06 19:47:23 -0700

    Cleaned up documentation files and removed coverage files

--- projects/gourleja/dominion/documentation.c
+++ projects/gourleja/dominion/documentation.c
@@ -1,5 +1,5 @@
 Jason Gourley
-gourleja
+gourleja@oregonstate.edu
 CS362-400
 Assignment 2 - documentation.c
 
@@ -11,10 +11,10 @@ Smithy Card
 
     name:           Smithy
     value:          0
-	purchase cost:  4 coins
-	card type:      Action
+    purchase cost:  4 coins
+    card type:      Action
 
-	ability: 		When used during the player's action phase the player draws
+    ability:        When used during the player's action phase the player draws
                     3 cards from their own deck, placing the 3 cards into their
                     current hand of cards
 
@@ -23,10 +23,10 @@ Adventurer Card
 
     name:           Adventurer
     value:          0
-	purchase cost: 	6 coins
-	card type:		Action
+    purchase cost:  6 coins
+    card type:      Action
 
-	ability:        When used during the player's action phase the player reveals
+    ability:        When used during the player's action phase the player reveals
                     cards from their deck until 2 treasure cards are revealed.
                     The 2 treasure cards are added to the current hand of cards,
                     while the rest of the cards drawn are placed in the discard pile.
@@ -41,15 +41,15 @@ Methods
 
 discardCard()
 
-	signature:		int discardCard(int handPos, int currentPlayer, struct gameState *state,  int trashFlag);
+    signature:      int discardCard(int handPos, int currentPlayer, struct gameState *state,  int trashFlag);
 
-	precondition:   Method is passed the player, the card's position in their
+    precondition:   Method is passed the player, the card's position in their
                     hand, current game state, and a trash flag
 
-	postcondition:  The passed card is removed from the player's hand and placed
+    postcondition:  The passed card is removed from the player's hand and placed
                     into the played cards pile or discard pile.
 
-	description:    The method will move a player's card from their hand to either
+    description:    The method will move a player's card from their hand to either
                     the played cards pile or discard pile based on the passed
                     trashFlag.  For flag values, trashFlag == 0 move the card to
                     the played pile and where trashFlag > 0 the card is discarded.
@@ -59,14 +59,14 @@ discardCard()
 
 updateCoins()
 
-	signature:		int updateCoins(int player, struct gameState *state, int bonus);
+    signature:      int updateCoins(int player, struct gameState *state, int bonus);
 
-	preconditions:  Method is passed the current player, current game state,
+    preconditions:  Method is passed the current player, current game state,
                     and any bonus coins from action cards
 
-	postconditions: The player's coins from their current hand and any bonus
+    postconditions: The player's coins from their current hand and any bonus
                     coins are added to the played field.
 
-	description:    The game state coins are first reset to zero.  The player's
+    description:    The game state coins are first reset to zero.  The player's
                     current hand is traversed and any available treasure cards
                     are added to the player's played field as well as any bonus coins.
