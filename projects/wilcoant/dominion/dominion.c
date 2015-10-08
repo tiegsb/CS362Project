b0fd04b312d742a303bbaade6cc3ae9eac8933d6
Anthony Wilcox <wilcoant@onid.oregonstate.edu>
2015-10-08 14:24:05 -0700

    completed documentation.c and introduced one bug in dominion.c

--- projects/wilcoant/dominion/dominion.c
+++ projects/wilcoant/dominion/dominion.c
@@ -830,7 +830,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 		
     case smithy:
       //+3 Cards
-      for (i = 0; i < 3; i++)
+      for (i = 0; i <= 3; i++)
 	{
 	  drawCard(currentPlayer, state);
 	}
