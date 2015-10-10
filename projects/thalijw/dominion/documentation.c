39382da97760fddc42986367066faf2194ded73f
Wisam82 <wisam.k.thalij@gmail.com>
2015-10-10 12:50:48 -0700

    Documentation.c added

--- projects/thalijw/dominion/documentation.c
+++ projects/thalijw/dominion/documentation.c
@@ -1,31 +1,43 @@
-Billy Kerns
-CS 362
-Documentation
+CS_362_Software Engineering
+Assignment #2
+thalijw
 
-smithy:
-	The smithy card adds three cards to the hand of the player who played the smithy card.  Once this is completed the card is
-	discarded from the player''s hand and goes into their played card pile.  This is done with a for loop that runs three times,
-	calling drawCard() on each iteration, and then calling the discardCard() function after exiting the loop.
+documentation about Smithy and Adventurer Cards:
 
-adventurer:
-	The adventurer card allows the player who plays the card to go through their deck until two treasure cards are found.  These
-	treasure cards are put into the player's hand while the revealed cards are placed in the played	card pile.  This is accolmplished
-	by running a while loop that runs while the variable drawntreasure is less than two.  In the while loop it checks to make sure
-	the player has more than one card in their deck, if not the played card pile is shuffled and added to the player's deck.  Then a 
-	card is drawn with the drawcard() function.  The variable cardDrawn is set to the top card of the player's hand.  The cardDrawn
-	variable is checked to see if it is a treasure card.  If it is then the drawntreasure variable is incremented.  Else the cardDrawn
-	is added to temp hand at z and the top card is removed.  z is incremented and the while loop goes back to the top.  After the while
-	loop is exited another while loop is started that gets rid of the non treasure cards that were drawn.
+Smithy:
+
+The Action of this card is +3 adding three new cards to the current player hand and discarding the smithy card in the end of the action.
+In the cardEffect()/case smithy code, it starts with a for loop that runs for 3 times, at each time it calls the function drawCard() 
+passing it the current player and the state of the game, it draws three cards and adds it to the current player hand, then it calls 
+the function discardCard() to discard the Smithy card from the player hand, passing it the hand position, the player, state of the game
+and index 0.
+
+Adventurer:
+
+This card makes the player to reveal cards from his deck unitl he reveals two treasure cards, then add these treasure cards to his hand,
+and discard the other revealed cards to the discarded pile, the code will keep track of those treasure card count in the drawntreasure variable.
+In the cardEffect()/case adventurer code, it starts with a while loop that runs while the number of treasure cards revealed < 2, it first checkes
+the player deck, if its empty, then it calls shuffle() to shuffle the players discarded cards and add them to the player card pile, then it calls 
+drawCard() to draw a card from the card play pile, then it adds that card to the top of the player's hand using the cardDrawn variable.
+The next step is checking if the card drawn is a treasure card(copper, silver or gold), if it is then we increment the drawntreasure variable by 1,
+if it is not a treasure card, then we added it to a temphand list and keep track of how many cards added using "z" index, then we remove it from 
+the top of the player's hand.
+After we found our two treasure cards and exit the above loop we enter another loop that removes the drawn cards that was not a treasure cards. 
+
 
 discardCard():
-	The discardCard() function will discard a card when called.  This is done by first entering an if statement that checks if trashFlag
-	has been set.  If it has not been set then the card is added to the played pile and playedCardCount is incremented.  Then the played
-	card is set to -1.  Another if block is entered which first checks if the card is the last card in the array.  If it is, the player's 
-	hand count is decremented.  Else if the player only has one card in their hand the player's hand count is decremented.  If neither of 
-	these conditions are met then the discarded card is replaced with last card, the last card is set to -1, and handCount is decremented.
+
+This function will discard a card and added to the player's played pile (discarded pile), it takes a hand position, the current player, the game state
+and the trashFlag (a flag to check if the card has been added to the played pile).
+First the function will check the trashFlag, if the card is not trashed, then it will add it to the player's discarded pile, and incremented the number 
+of the cards in this pile, then we set the played card's value to -1 (set it as played) and then remove that card from the player's hand by executing an
+if statement that first checks if it was the last card in hand, then it reduces the number of cards in hand by one, or if there is one card left in hand,
+then it also reduces the number of cards in hand by one, or in case there is more that one card in hand, it will replace the discarded card with the last one
+in hand, and set it's value to -1 (played) then decreased the count of the hand cards by 1.
 
 updateCoins():
-	The updateCoins() function uses a for loop and few if statements to update the amount of coins a player has in their hand.  This is 
-	accolmplished by first setting coins equal to 0 then entering the for loop.  Once in the for loop if statements check to if the card
-	is a copper, silver, or gold and if it is the appropriate amount of coins are added to the state.  After this has been done a user's 
-	bonus is added to the total amount of coins.
+
+This function will update the number of coins the playes have each turn. it will first sets the coins count in the game state to Zero to start with empty variable.
+Then it will enter a for loop that goes through the player's hand cards and checkes whether it is a "Copper", then it adds 1 to the coin's count, or if it is a "Silver",
+then it adds 2 coins to the count or if it is a "Gold", then it adds 3 coins to the count. at the end it will add to the count any bonus coins the player has ( this number
+is passed as an argument to the function).
