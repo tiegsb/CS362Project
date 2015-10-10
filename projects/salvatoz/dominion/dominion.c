802a98809635ae18f5bc2d95e57849c54cedc3a8
zanesalvatore <zanesalvatore@gmail.com>
2015-10-10 16:14:49 -0700

    throw error if newly-handled cards aren't handled

--- projects/salvatoz/dominion/dominion.c
+++ projects/salvatoz/dominion/dominion.c
@@ -663,32 +663,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3,
   // uses switch to select card and perform actions
   switch (card) {
   case adventurer:
-    while (drawntreasure < 2) {
-      if (state->deckCount[currentPlayer] < 1) { // if the deck is empty we
-                                                 // need to shuffle discard
-                                                 // and add to deck
-        shuffle(currentPlayer, state);
-      }
-      drawCard(currentPlayer, state);
-      cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer] -
-                                             1]; // top card of hand is most
-                                                 // recently drawn card.
-      if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-        drawntreasure++;
-      else {
-        temphand[z] = cardDrawn;
-        state->handCount[currentPlayer]--; // this should just remove the top
-                                           // card (the most recently drawn
-                                           // one).
-        z++;
-      }
-    }
-    while (z - 1 >= 0) {
-      state->discard[currentPlayer][state->discardCount[currentPlayer]++] =
-          temphand[z - 1]; // discard all cards in play that have been drawn
-      z = z - 1;
-    }
-    return 0;
+    fprintf(stderr, "fail: tried to handle adventurer from cardEffect switch");
+    return -1;
 
   case council_room:
     //+4 Cards
@@ -821,14 +797,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3,
     return 0;
 
   case smithy:
-    //+3 Cards
-    for (i = 0; i < 3; i++) {
-      drawCard(currentPlayer, state);
-    }
-
-    // discard card from hand
-    discardCard(handPos, currentPlayer, state, 0);
-    return 0;
+    fprintf(stderr, "fail: tried to handle smithy from cardEffect switch");
+    return -1;
 
   case village:
     //+1 Card
