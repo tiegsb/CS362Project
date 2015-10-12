f7c9a51805f4ce5690e1e47a294cdfef12cfa91e
Larissa Hahn <hahnl@onid.oregonstate.edu>
2015-10-11 21:54:53 -0500

    original refactor

---
+++ projects/hahnl/dominion/refactor.c
@@ -0,0 +1,26 @@
+/* Author: Larissa Hahn
+File name: refactor.c
+Due date: 10-11-15
+E-mail: hahnl@oregonstate.edu
+
+Note: Refactor code starts on line 1170
+*/
+
+/*
+//// Smithy Card - void smithCard ()
+
+
+//// Adventurer Card - void adventurerCard ()
+
+
+//// Great Hall Card - void greatHallCard ()
+
+
+//// Village Card - void villageCard ()
+
+
+//// Council Room Card - void councilRoomCard ()
+
+
+
+*/
