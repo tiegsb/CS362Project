486e6541314e920d9269ebe688cb467e903f12b0
Tony Huynh <huynhton@flip1.engr.oregonstate.edu>
2015-10-12 19:36:12 -0700

    test

--- projects/huynhton/dominion/dominion.c
+++ projects/huynhton/dominion/dominion.c
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <stdlib.h>
 
+// 
+// testing change. 
 int compare(const void* a, const void* b) {
   if (*(int*)a > *(int*)b)
     return 1;
@@ -12,6 +14,11 @@ int compare(const void* a, const void* b) {
     return -1;
   return 0;
 }
+
+int dummymethod(){
+	return 0;
+}
+
 
 struct gameState* newGame() {
   struct gameState* g = malloc(sizeof(struct gameState));
