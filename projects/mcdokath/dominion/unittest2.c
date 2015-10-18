f3b5740da405f98370f8f15f62e841c8b2d4c347
mcdokath <mcdokath@oregonstate.edu>
2015-10-17 18:29:08 -0700

    Added unittest2.c

---
+++ projects/mcdokath/dominion/unittest2.c
@@ -0,0 +1,54 @@
+/* Name: Kathryn McDonald
+ * Class: CS 362
+ * File name: unittest1.c
+ * Purpose: Tests the "shuffle" function in dominion.c */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <assert.h>
+
+// to remove printfs, set PRINT_TEST to 0
+#define PRINT_TEST 1
+
+// random seed
+#define RANDOM_SEED 5
+
+int main() {
+  
+  /* set up game state */
+  struct gameState G;
+  int k[10] = {adventurer, gardens, embargo, village, minion, 
+    mine, cutpurse, sea_hag, tribute, smithy};
+    
+  /* set up hand */
+  initializeGame(2, k, RANDOM_SEED, &G);
+  
+  printf("Setup complete.\n");
+  
+  /* test */
+  printf("Testing shuffle()...\n");
+  
+  /* shuffle Player 0's deck */
+  int shuffleResult = shuffle(0, &G);
+  
+  /* check result */
+#if (PRINT_TEST == 1)
+  printf("Shuffle Player 0.\nResult: %d, Expected: %d\n",shuffleResult, 0);
+#endif
+  assert(shuffleResult == 0);
+  
+  /* shuffle Player 1's deck */
+  shuffleResult = shuffle(1, &G);
+  
+  /* check result */
+#if (PRINT_TEST == 1)
+  printf("Shuffle Player 1.\nResult: %d, Expected: %d\n",shuffleResult, 0);
+#endif
+  assert(shuffleResult == 0);
+    
+  printf("All tests passed!\n");
+  
+  return 0;
+}
