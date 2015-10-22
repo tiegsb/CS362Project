5acf84d649b55ee96743016ba8650ebfdbdf7318
mosspilot <mosspilot@gmail.com>
2015-10-21 21:30:51 -0700

    Added all 4 unit tests and makefile changes. Added cardtest1, but it needs work.

--- projects/mossjef/dominion/unittest2.c
+++ projects/mossjef/dominion/unittest2.c
@@ -27,7 +27,7 @@ int main() {
 
 void testfullDeckCount () {
 	struct gameState G;
-	  
+	int fail = 0;	  
 	G.numPlayers = 2;
 	int player = 0;
 	  
@@ -55,6 +55,7 @@ void testfullDeckCount () {
 		printf("fullDeckCount(): PASS estate card expected 2, counted %d\n", estCount);
 	} else {
 		printf("fullDeckCount(): FAIL estate card expected 2, counted %d\n", estCount);
+		fail = 1;
 	}
 
 	int councilCount = fullDeckCount(player, council_room, &G);
@@ -64,6 +65,7 @@ void testfullDeckCount () {
 		printf("fullDeckCount(): PASS council_room card expected 0, counted %d\n", councilCount);
 	} else {
 		printf("fullDeckCount(): FAIL council_room card expected 0, counted %d\n", councilCount);
+		fail = 1;
 	}
 
 #if(NOISY_TEST == 1)
@@ -81,6 +83,7 @@ void testfullDeckCount () {
 		printf("fullDeckCount(): PASS estate card expected 3, counted %d\n", estCount);
 	} else {
 		printf("fullDeckCount(): FAIL estate card expected 3, counted %d\n", estCount);
+		fail = 1;
 	}
 
 	councilCount = fullDeckCount(player, council_room, &G);
@@ -90,6 +93,7 @@ void testfullDeckCount () {
 		printf("fullDeckCount(): PASS council_room card expected 1, counted %d\n", councilCount);
 	} else {
 		printf("fullDeckCount(): FAIL council_room card expected 1, counted %d\n", councilCount);
+		fail = 1;
 	}
 
 	
@@ -109,6 +113,7 @@ void testfullDeckCount () {
 		printf("fullDeckCount(): PASS council_room card expected 2, counted %d\n", councilCount);
 	} else {
 		printf("fullDeckCount(): FAIL council_room card expected 2, counted %d\n", councilCount);
+		fail = 1;
 	}
 
 	int smithyCount = fullDeckCount(player, smithy, &G);
@@ -117,7 +122,10 @@ void testfullDeckCount () {
 		printf("fullDeckCount(): PASS smithy card expected 4, counted %d\n", smithyCount);
 	} else {
 		printf("fullDeckCount(): FAIL smithy card expected 4, counted %d\n", smithyCount);	
+		fail = 1;
 	}
 
-	
+	if(fail == 0) {
+		printf("fullDeckCount(): OK - ALL TESTS PASSED\n\n");	
+	}
 }
