ce1d8cb913b9827bfa5626d372606d377ff44468
Ian Kronquist <iankronquist@teleport.com>
2015-10-24 07:46:55 -0700

    Add bugs discovered in council room and villager

--- projects/kronquii/bugs.c
+++ projects/kronquii/bugs.c
@@ -3,3 +3,5 @@
 3. The adventurer card is not discarded. There are likely other bugs lurking in
 the adventurer implementation, but my tests fail early.
 4. The smithy card does not draw the right number of cards.
+5. The council room does not add another buy.
+6. The arguments to the villagerEffect function are passed in the wrong order.
