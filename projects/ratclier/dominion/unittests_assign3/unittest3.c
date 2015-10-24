c41d414129f8d0331d04b543404e23133a98b590
Erik Ratcliffe <ratclier@oregonstate.edu>
2015-10-24 16:45:42 -0700

    Updates to unittest2 and unittest3

--- projects/ratclier/dominion/unittests_assign3/unittest3.c
+++ projects/ratclier/dominion/unittests_assign3/unittest3.c
@@ -5,31 +5,7 @@
 
 
 // Test the updateCoins() function
-/*
-  //reset coin count
-  state->coins = 0;
-
-  //add coins for each Treasure card in player's hand
-  for (i = 0; i < state->handCount[player]; i++)
-    {
-      if (state->hand[player][i] == copper)
-        {
-          state->coins += 1;
-        }
-      else if (state->hand[player][i] == silver)
-        {
-          state->coins += 2;
-        }
-      else if (state->hand[player][i] == gold)
-        {
-          state->coins += 3;
-        }       
-    }   
-
-  //add bonus
-  state->coins += bonus;
-*/
-
+//
 int testUpdateCoins(int player, struct gameState *state, int bonus)
 {
     struct gameState *origState;  // copy of game state
@@ -44,31 +20,42 @@ int testUpdateCoins(int player, struct gameState *state, int bonus)
     //
     updateCoins(player, state, bonus);
 
+    printf("Player %d coins:\n", player);
+
     for (i = 0; i < state->handCount[player]; i++)
     {
         if (state->hand[player][i] == copper)
         {
             coinCount += 1;
+            printf("\tOne copper (1)\n");
         }
         else if (state->hand[player][i] == silver)
         {
             coinCount += 2;
+            printf("\tOne silver (2)\n");
         }
         else if (state->hand[player][i] == gold)
         {
             coinCount += 3;
+            printf("\tOne gold (3)\n");
         }       
     }   
+
+    printf("Plus optional bonus: %d\n", bonus);
     
     coinCount += bonus;
 
+    // Note that state->coins is not tied to any player. It is a temporary
+    // placeholder for coin information. Original values in origState are
+    // not used in determining new state->coins values.
+    //
     if(state->coins == coinCount)
     {
-        printf("updateCoins: PASS game state holds correct number of coins.\n");
+        printf("updateCoins: PASS game state holds correct number of coins (%d)\n", state->coins);
     }
     else
     {
-        printf("updateCoins: FAIL game state holds incorrect number of coins.\n");
+        printf("updateCoins: FAIL game state holds incorrect number of coins (%d)\n", state->coins);
     }
 
     printf("\nupdateCoins: Changes to game state:\n----------------------------------------\n");
@@ -96,7 +83,8 @@ int main(int argc, char *argv[])
 
     printf("\n");
 
-    // Discard a trashed card
+    // Player 0, bonus 5
+    //
     printf("Testing: Player 0's coins, 5 bonus...\n");
     player = 0;
     bonus = 5;
@@ -107,10 +95,11 @@ int main(int argc, char *argv[])
     state = newGame();
     initializeGame(numPlayers, kingdomCards, randomSeed, state);
 
-    // Discard a non-trashed card
+    // Player 1, bonus 3
+    //
     printf("Testing: Player 1's coins, 3 bonus...\n");
-    player = 0;
-    bonus = 3;
+    player = 4;
+    bonus = 8;
     testUpdateCoins(player, state, bonus);
 
     return 0;
