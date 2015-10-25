c1e0461b8e8724948c7b05032eda6f50e75a0973
Scott Milton <miltons@oregonstate.edu>
2015-10-24 20:40:03 -0700

    Replace assertions in test files with if-else statements. Add arguments to function calls.

--- projects/miltons/dominion/cardtest4.c
+++ projects/miltons/dominion/cardtest4.c
@@ -1,12 +1,21 @@
 // tests Mine card
+// int mineEffect(int currentPlayer, struct gameState *state, int handPos, int choice1, int choice2)
 
 #include<stdio.h>
 #include "dominion.h"
+//#include "dominion_helpers.h"
+//#include "rngs.h"
 
 int main(int argc, char *argv[])
 {
-    int retVal = mineEffect();
-    assert(retVal == 0) printf("Mine card is working!\n");
-
+    int retVal = mineEffect(0, gameState, 0, 0, 0);
+    if (retVal == 0)
+    {
+        printf("Mine card: PASS\n");
+    }
+    else
+    {
+        printf("Mine card: FAIL\n");
+    }
     return 0;
 }
