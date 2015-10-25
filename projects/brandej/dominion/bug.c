262b6ef79b94e423a65bc562041537aab2e070b6
Josh Brande <brandej@oregonstate.edu>
2015-10-25 14:52:08 -0700

    cardtest1.c & various fixes

--- projects/brandej/dominion/bug.c
+++ projects/brandej/dominion/bug.c
@@ -8,12 +8,18 @@ unittest2:
 unittest3:
     scoreFor() contains a bug. The gardens card causes errors due to passing
     0 to numHandsCards for the card argument and therefore not actually
-    counting all cards.
+    counting all cards.s
 
 unittest4:
     Since getWinners() uses scoreFor(), it would technically have the same issue listed above.
     My tests navigated around this to test the rest of the funnctionality and no
     bugs were found.
+
+cardtest1:
+    Testing the Adventure card showed 2 bugs. It will place up to 3
+    treasure cards in hand since the while loop is <= instead of <. The funciton will also
+    return hand count of -1 if no adventure cards are found. This last one might be an error
+    or just undefined behavior.
 
 
 
