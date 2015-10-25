49b7ba478d58c76339260fec7daaf1a3ecda6daf
zanesalvatore <zanesalvatore@gmail.com>
2015-10-25 12:57:51 -0700

    document discovered bugs for cardtest1 and 2, stub cardtest3 and 4

--- projects/salvatoz/dominion/bug1.c
+++ projects/salvatoz/dominion/bug1.c
@@ -1,5 +1,12 @@
 /*
+None of the unittest* files reveal any bugs.
+
 smithy: Doesn't lead to a correct number of cards in hand after use.
-
-
+adventurer: always segfaults
+council room: 
+mine:
 */
+
+int main(int argc, char** argv) {
+  return 0;
+}
---
+++ projects/salvatoz/dominion/cardtest3.c
@@ -0,0 +1,1 @@
+/* council room test */
