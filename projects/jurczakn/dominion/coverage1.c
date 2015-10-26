c5ed61993da1c4bdf82604955368cc42ad9b2c85
jurczakn <jurczakn@onid.oregonstate.edu>
2015-10-26 01:47:20 -0500

    unittest3

--- projects/jurczakn/dominion/coverage1.c
+++ projects/jurczakn/dominion/coverage1.c
@@ -1,2 +1,5 @@
+scoreFor(): Function has complete coverage.
 
 getWinners(): line 470 is never run, because there are never unused players, the unittest2 always runs with 4 players.  It should be changed to run with a varied number of players.
+
+isGameOver(): Function has complete coverage.
