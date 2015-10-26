7b39004b4ebdf9d3dc2139f210b5d4baf8b4f381
Ryan Kitchen <nayrki@epsilon>
2015-10-25 23:55:26 -0700

    Assignment 3 early commit

---
+++ projects/kitchenr/dominion/bug1.c
@@ -0,0 +1,11 @@
+Bugs found in unit testing:
+
+Unit test 1: initializeGame
+  
+  All players hands should be drawn at the beginning of the game, only player 1's hand is drawn
+    -> is commented out in code? 
+
+  Does not check if selected kingdom cards are actually kingdom cards, accepted copper as a kingdom card
+
+
+  
