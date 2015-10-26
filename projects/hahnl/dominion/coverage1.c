2c9cebb48b9874ca06f70dc715b63d6dbb434733
Larissa Hahn <hahnl@onid.oregonstate.edu>
2015-10-26 02:13:17 -0500

    updated

---
+++ projects/hahnl/dominion/coverage1.c
@@ -0,0 +1,6 @@
+/*
+Average coverage: 26%
+I definitely could improve my coverage; however, my tests were meant to test
+very specific effects of each card as well as the purpose or meanings of
+the functions. So it was a very narrow test suite.
+/*
