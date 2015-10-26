86cd881349eb8b2ad5e353d92a866734b151ef13
Josh Brande <brandej@oregonstate.edu>
2015-10-25 18:41:08 -0700

    added bug

--- projects/brandej/dominion/bug.c
+++ projects/brandej/dominion/bug.c
@@ -3,7 +3,7 @@ unittest1:
     This means sometimes when a game is over, it might not register.
 
 unittest2:
-    discardCard() no bugs found.
+    discardCard() no bugs found based on written functionality within this test suite.
 
 unittest3:
     scoreFor() contains a bug. The gardens card causes errors due to passing
@@ -16,10 +16,20 @@ unittest4:
     bugs were found.
 
 cardtest1:
-    Testing the Adventure card showed 2 bugs. It will place up to 3
+    Testing the Adventure card showed 3 bugs. It will place up to 3
     treasure cards in hand since the while loop is <= instead of <. The funciton will also
     return hand count of -1 if no adventure cards are found. This last one might be an error
-    or just undefined behavior.
+    or just undefined behavior. In addition, the card does not discard itself after being played.
+
+cardtest2:
+    Testing smithy card showed 1 major bug with the dsicardCard funciton. That funciton does not place
+    discarded cards in the discard pile or increment the discardCount.
+
+cardtest3:
+    tested the Village card. No bugs were found.
+
+cardtest4:
+    I was traveling for work the past 2 weeks and ran out of time.
 
 
 
