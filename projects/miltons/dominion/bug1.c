b9375ccc360a63bc7c53c83f5d1628505bb6f968
Scott Milton <miltons@oregonstate.edu>
2015-10-25 20:24:03 -0700

    Touch up to unittest2.c. Add descriptions to bug1.c and coverage1.c.

---
+++ projects/miltons/dominion/bug1.c
@@ -0,0 +1,15 @@
+
+/*
+
+TEST NAME:      BUGS FOUND
+--------------------------
+cardtest1.c     none
+cardtest2.c     none
+cardtest3.c     none
+cardtest4.c     test reveals one or more run-time errors, returns exit value of 1
+unittest1.c     none
+unittest2.c     none
+unittest3.c     none
+unittest4.c     test reveals one or more run-time errors, returns exit value of 1
+
+*/
