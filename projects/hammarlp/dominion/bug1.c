d4c34385a0f59f66faa3c80d5ca3095afa7e0540
Hampus Hammarlund <hampus_hammarlund@hotmail.com>
2015-10-25 20:27:08 -0700

    Finished unittest and cardtest

---
+++ projects/hammarlp/dominion/bug1.c
@@ -0,0 +1,2 @@
+//cardtest2 fails because of the bug I introduced for assignment 2 making it so you draw/dicard incorrectly 
+//cardtest3 number of actions check fails because I introduced a bug for assignment 2 setting the wrong amount of actions.
