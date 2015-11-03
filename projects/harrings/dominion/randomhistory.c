5570bd4b50fe66937e1b34e6591efcf8cbe9d2d3
Harrings <sean.harrington2@gmail.com>
2015-11-03 12:45:10 -0800

    finished random tests

---
+++ projects/harrings/dominion/randomhistory.c
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+Gcov of Randomtest adventurer.c
+File 'dominion.c'
+Lines executed:26.08% of 579
+dominion.c:creating 'dominion.c.gcov'
+
+File 'randomtestadventurer.c'
+Lines executed:90.63% of 64
+Branches executed:72.73% of 22
+Taken at least once:50.00% of 22
+Calls executed:100.00% of 14
+randomtestadventurer.c:creating 'randomtestadventurer.c.gcov'
+
+
+Gcov of Randomtestcard.c
+File 'dominion.c'
+Lines executed:26.08% of 579
+dominion.c:creating 'dominion.c.gcov'
+
+File 'randomtestcard.c'
+Lines executed:90.32% of 62
+Branches executed:100.00% of 20
+Taken at least once:85.00% of 20
+Calls executed:100.00% of 9
+randomtestcard.c:creating 'randomtestcard.c.gcov'
