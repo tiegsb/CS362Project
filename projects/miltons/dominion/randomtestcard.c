7ec17252ca5472dcb617c291262f20908a8d456a
Scott Milton <miltons@oregonstate.edu>
2015-11-07 14:50:33 -0800

    Import basic code for Smithy and Adventurer cards.

---
+++ projects/miltons/dominion/randomtestcard.c
@@ -0,0 +1,25 @@
+// tests Smithy card
+// int smithyEffect(int currentPlayer, struct gameState *state, int handPos)
+
+// test / assert against state of game before and after smithy is played
+
+
+#include <stdio.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+//#include "rngs.h"
+
+int main(int argc, char *argv[])
+{
+    struct gameState testState;
+    int retVal = smithyEffect(0, &testState, 0);
+    if (retVal == 0)
+    {
+        printf("Smithy card: PASS\n");
+    }
+    else
+    {
+        printf("Smithy card: FAIL\n");
+    }
+    return 0;
+}
