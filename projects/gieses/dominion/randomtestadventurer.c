8c6b82f4883665b6a255dae05a1b019bacefbb2e
Savannah R Giese <savannahrgiese1@Savannahs-Air.home>
2015-11-06 21:26:47 -0800

    Added random test adventurer and updated make file

--- projects/gieses/dominion/randomtestadventurer.c
+++ projects/gieses/dominion/randomtestadventurer.c
@@ -7,27 +7,31 @@
 
 #define NOISY_TEST 0
 
-//test smithyCard function
+//test adventurer function
 int main(){
 	int i, j;
 	int numPlayers = 2;
 	int p = 0;
 	int seed = 1000;
 	int k[10] = {adventurer, council_room, feast, gardens, mine
-	           , remodel, smithy, village, baron, great_hall};
+	           , remodel, smithy, copper, gold, silver};
 	struct gameState G;
 	int cardFailedHand = 0;
 	int cardFailedDiscard = 0;
 	int cardFailedPlayed = 0;
 	srand(time(NULL));
 	int randomNum;
+	int countTreasure;
 	
 	memset(&G, 23, sizeof(struct gameState));
 	
 	for(i = 0; i < 500; i++){
 		initializeGame(numPlayers, k, seed, &G);
 		
-		//randomize how many cards are in players hand, always having at least 1 smithy card
+		//reset treasure count
+		countTreasure = 0;
+		
+		//randomize how many cards are in players hand, always having at least 1 adventurer card
 		G.handCount[p] = rand() % 11 + 1;
 		#if (NOISY_TEST == 1)
 		printf("Hand count is: %d\n", G.handCount[p]);
@@ -42,8 +46,8 @@ int main(){
 		int preHand = G.handCount[p];
 		int preDiscard = G.discardCount[p];
 		
-		//add smithy card to ensure it is used
-		G.hand[p][0] = 13;
+		//add adventurer card to ensure it is used
+		G.hand[p][0] = 7;
 		
 		//randomly insert cards into player pile
 		for (j = 1; j < G.handCount[p]; j++){
@@ -68,24 +72,48 @@ int main(){
 			printf("Discard card: %d and kingdom card: %d\n", G.discard[p][i], k[randomNum]);
 			#endif
 		}
-		
+	
 		#if (NOISY_TEST == 1)
-		printf("Pre player hand count: %d\n", preHand);
-		printf("Pre player discard count: %d\n", preDiscard);
-		printf("Pre played card count: %d\n", G.playedCardCount);
+		printf("Pre player hand count: %d\n", G.handCount[p]);
+		printf("Pre player discard count: %d\n", G.discardCount[p]);
+		printf("Pre discard count: %d\n", G.discardCount[p]);
 		#endif
 		
-		smithyCard(&G, 0);
-		
+		adventurerCard(&G);
+	
 		#if (NOISY_TEST == 1)
 		printf("Post player hand count: %d\n", G.handCount[p]);
 		printf("Post player discard count: %d\n", G.discardCount[p]);
-		printf("Post played card count: %d\n", G.playedCardCount);
+		printf("Post discard count: %d\n", G.discardCount[p]);
 		#endif
+	
+		//check last card added to player hand
+		if(G.hand[p][G.handCount[p] - 1] == copper || G.hand[p][G.handCount[p] - 1] == silver || G.hand[p][G.handCount[p] - 1] == gold){
+			#if (NOISY_TEST == 1)
+			printf("Found a card!\n");
+			countTreasure++;
+			#endif
+		}else{
+			#if (NOISY_TEST == 1)
+			printf("Didn't find a card!\n");
+			#endif
+		}
 		
-		//pre hand + 3 should be the same as the hand after the card is played
-		//since we gain 3 cards playing the smithy card
-		if(preHand + 3 == G.handCount[p]){	
+		//check second to last card added to player hand
+		if(G.hand[p][G.handCount[p] - 2] == copper || G.hand[p][G.handCount[p] - 2] == silver || G.hand[p][G.handCount[p] - 2] == gold){
+			#if (NOISY_TEST == 1)
+			printf("Found a card!\n");
+			countTreasure++;
+			#endif
+		}else{
+			#if (NOISY_TEST == 1)
+			printf("Didn't find a card!\n");
+			#endif
+		}
+		
+		//pre hand should be no more than the hand + 2 after the card is played 
+		//since we gain at most 2 cards
+		if(preHand <= G.handCount[p] + 2){	
 			#if (NOISY_TEST == 1)
 			printf("Test passed!\n");
 			#endif
@@ -96,9 +124,9 @@ int main(){
 			cardFailedHand++;
 		}
 		
-		//pre discard count - 3 should be the same as the discard count after
-		//the card is played since the player gains 3 cards
-		if(preDiscard - 3 == G.discardCount[p]){
+		//pre discard count should equal or greater than the discard pile
+		//after the card is played
+		if(preDiscard >= G.discardCount[p] - 2){
 			#if (NOISY_TEST == 1)
 			printf("Test passed!\n");
 			#endif
@@ -120,6 +148,19 @@ int main(){
 			printf("Test passed!\n");
 			#endif
 		}
+		
+		//check if the number of treasure cards added is correct
+		if(countTreasure >= 0 && countTreasure <= 2){
+			#if (NOISY_TEST == 1)
+			printf("Test passed!\n");
+			printf("Card count: %d\n", countTreasure);
+			#endif		
+		}else{
+			#if (NOISY_TEST == 1)
+			printf("Test failed!\n");
+			printf("Card count: %d\n", countTreasure);
+			#endif
+		}
 	}
 	
 	#if (NOISY_TEST == 1)
@@ -128,4 +169,3 @@ int main(){
 	
 	return 0;
 }
-    
