e06ad34105df23e3183d4cf6ea6b2cd31e3555f1
Larissa Hahn <hahnl@onid.oregonstate.edu>
2015-11-07 07:31:25 -0600

    Adjusted tests to reduce wait time
    Adjusted randomtestadventure.c to reduce the “make” command wait time…
    went from 10 tests to 2 tests for the grader to compile

--- projects/hahnl/dominion/randomtestadventurer.c
+++ projects/hahnl/dominion/randomtestadventurer.c
@@ -7,6 +7,9 @@ time maybe 5-7 minutes or so due to the bugs in the files
 which cause the tests to run slow. But they will
 eventually complete with a great test report.
 
+UPDATE: I will adjust the number here to make it take
+less time to run per Piazza post.
+
 ******************************************************/
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -53,7 +56,7 @@ int main (int argc, char** argv) {
 
   //Tests begin
   printf("\n\n--------------------------------------\nrandomtestadventurer.c -- Tests Begin\n--------------------------------------\n\n");
-  for (i = 0; i < 10; i++) {  //Reduced number due to bugs causing long wait times
+  for (i = 0; i < 2; i++) {  //Reduced number due to bugs causing long wait times
     //Initialize game with Adventurer Card
     memset(&g, 23, sizeof(struct gameState));
     newGame = initializeGame(2, k, 1000, &g);
@@ -107,7 +110,7 @@ int main (int argc, char** argv) {
     }
 
     if (failure == true) {
-      printf("^^^^^^^ Test Number %d out of 10 ^^^^^^^\n\n", i+1);
+      printf("^^^^^^^ Test Number %d out of 2 ^^^^^^^\n\n", i+1);
       totalFailures++;
     }
   }
@@ -118,7 +121,7 @@ int main (int argc, char** argv) {
     printf("  ---- TEST FAILURE STATUS REPORT ----\n");
     printf("  ----       Adventurer Card      ----\n");
     printf("****************************************\n");
-    printf("Total Test Failures:      %d/10\n", totalFailures);
+    printf("Total Test Failures:      %d/2\n", totalFailures);
     printf("Discard Card Errors:      %d\n", discardErrors);
     printf("Draw Card Errors:         %d\n", drawErrors);
     printf("Treasure Card Errors:     %d\n", treasureErrors);
