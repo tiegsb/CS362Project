ef8c0f461e27a73381a3983df4400b979c2b5eb1
chanal <chanal@onid.oregonstate.edu>
2015-11-07 01:34:23 -0800

    Corrected a typo in randomhistory.c

--- projects/chanal/dominion/randomhistory.c
+++ projects/chanal/dominion/randomhistory.c
@@ -14,7 +14,7 @@ randomtestadventurer (tests adventurer)
 		Branches executed: 100.00% of 26
 		Taken at least once: 76.92% of 26
 		Calls executed: 100.00% of 19
-	Lines executed:	23.97% of 580 in dominion.c
+	Lines executed:	21.21% of 580 in dominion.c
 
 	For the random tester for adventurer, I referred to and used much of my 
 	unit test that was developed for the adventurer card portion. The changes
@@ -28,7 +28,7 @@ randomtestadventurer (tests adventurer)
 	if the correct number of treasure cards are drawn. Those were the two tests that were pass/fail and
 	a reported summary is proviuded at the end of the test. Both tests failed everytime since those were the 
 	bugs that I introduced into adventurer card that were being testing in the random tester.
-	The coverage for the randomtestadventurer was slightly lower in dominion.c (23.97% vs 25.00%), however, 
+	The coverage for the randomtestadventurer was slightly lower in dominion.c (21.21% vs 25.00%), however, 
 	the coverage for the the randomtestadventurer is overall higher than my unit test for the adventurer card
 	such as the lines executed, and calls executed. The branches executed is the same as before at 100.00%.
 	The taken at least once is slightly lower from 77.27% to 76.92% previously in the unit test. 
@@ -43,7 +43,7 @@ randomtestcard (tests smithy)
 		Branches executed: 100.00% of 18
 		Taken at least once: 77.78% of 18
 		Calls executed: 82.61% of 23
-	Lines executed: 21.21% of 580 in dominion.c
+	Lines executed: 23.97% of 580 in dominion.c
 
 	For the random tester for smithy, I referred to and used much of my unit test
 	that was developed for the smithy card portion. The changes made
@@ -59,7 +59,7 @@ randomtestcard (tests smithy)
 	and the post-hand state of the player.
 	Of the two tests, one always passed and the other always failed. This was because of the bug that
 	I introduced into the smithy card that was being tested in the random tester. 
-	The coverage for the randomtestcard is slightly lower in dominion.c (21.21% vs 23.45%) and
+	The coverage for the randomtestcard is slightly higher in dominion.c (23.97% vs 23.45%) and
 	the coverage for the randomtestcard is very similar to my unit test for the smithy card.
 	The taken at least once is greater than my unit test for smithy. The lines executed for randomtestcard
 	is less than my unit test because the non-printed lines are from the if-else statements and my error counter as seen below:
