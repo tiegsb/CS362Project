be26e411d572c3baaaf86c1b202fa149a6ce205c
Jibrahn <jasolomy@gmail.com>
2015-11-09 03:35:03 -0500

    working on random tester

--- projects/khouryji/dominion/refactor.c
+++ projects/khouryji/dominion/refactor.c
@@ -5,9 +5,7 @@ Changes to adventurer, smithy, village, steward, and outpost
 
 Adventurer changes:
 
-tempHand's array size is set to 4 instead of MAX_HAND.
-While loop condition set to drawntreasure<20 instead of <2.
-shuffle(currentPlayer,state) changed to shuffle(currentPlayer++,state)
+'shuffle(currentPlayer,state) changed to shuffle(currentPlayer++,state)
 [state->handCount[currentPlayer]-1] changed to [state->handCount[currentPlayer+1]-1]
 in line: cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1]; 
 
@@ -27,9 +25,7 @@ state->numActions = state->numActions + 2;
 changed to
 state->numActions = state->playedCardCount++ + 2;
 
-discardCard(handPos,
-changed to
-discardCard(handPos--,
+
 
 Steward changes:
 
