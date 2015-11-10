630d1382223b6554565c3fef3dec5d4dae26284b
Anne Lindenberg <lindenba@onid.orst.edu>
2015-11-10 10:12:07 -0800

    missed adding my randomhistory.c to my folder adding it now

---
+++ projects/lindenba/dominion/randomhistory.c
@@ -0,0 +1,7 @@
+Anne Lindenberg
+CS362
+Assignment 4
+
+I’m not exactly sure how to run the random test.  I conceptually understand, but I don’t know for sure. So for randomtestadventurer.c I checked the coins amount to see if there was any randomness that would occur in running the program. I initialized the game state seeded random players and ran through a bunch of iterations to see what was happening with the coins. If there was an error than print.  Running through I found no errors with tests running.
+
+For the adventure.c I ran random test on 3 game states to compare handCount, deckCount and each iteration calling on CardEffect().  Printed out each state of the hand.  This coverage probably isn’t the best way to go about.  This is where I am a little confused on the random test.
